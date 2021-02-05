0ac6ec0145151332e27cb8524e630ed182a0d047
Pierre Barbera <pierre.pbdas@gmail.com>
2021-02-05 18:44:15 +0100

    added epa-ng compilation, submodule setup, and test file unpacking to setup script

--- setup.sh
+++ setup.sh
@@ -8,12 +8,17 @@ die () {
 
 BASE=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
-# ensure epa-ng is here and properly cloned
-
-# same for PEWO
+# ensure submodules are here
+git submodule update --init --recursive
+[[ ! -f ${BASE}/epa-ng/Dockerfile ]] && die "epa-ng submodule not found"
+[[ ! -f ${BASE}/PEWO/INSTALL.sh ]] && die "PEWO submodule not found"
 
 # build epa-ng/-serial
-
+cd ${BASE}/epa-ng
+EPA_SERIAL=1 make -j $(nproc)
+mv bin/epa-ng bin/epa-ng-serial
+make update -j $(nproc)
+cd -
 
 # change the %LOCATION% text in the snakemake/pewo yaml files to the full current path
 for f in ${BASE}/configs/*.yaml
@@ -22,3 +27,7 @@ do
 done
 
 # unpack but keep relevant packed reference files
+for f in ${BASE}/datasets/*/reference.fasta.gz
+do
+	gunzip --force --keep $f
+done
